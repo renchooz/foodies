cd74a8fe902878787c33829025ac8cb74363a7cd
renchooz <rjsharma.rs967@gmail.com>
2025-04-07 19:47:51 +0530

    foodies

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext()
+
+export const AppContextProvider = ({children})=>{
+    const nevigate = useNavigate()
+    const [User, setUser] = useState(null)
+    const [isSeller, setisSeller] = useState(false)
+    const value = {nevigate,User,setUser,isSeller,setisSeller}
+          return <AppContext.Provider value={value}>
+            {children}
+          </AppContext.Provider>
+}
+export const useAppContext =()=>{
+    return useContext(AppContext)
+}
